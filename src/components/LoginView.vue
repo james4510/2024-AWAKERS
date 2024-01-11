9ac563ffe790d3f4d6fd25493f71a7ca34a520b8
james4510 <james451055@gmail.com>
2024-01-11 13:50:09 +0900

    2024.1.11 login page

--- src/components/LoginView.vue
+++ src/components/LoginView.vue
@@ -1,22 +1,27 @@
 <template>
     <div>
-        <!-- 뒤로가기 아이콘 -->
-        <img src="../assets/chevron-left.svg" alt="뒤로가기" @click="goBack" />
-        
         <!-- 로그인 문자열 -->
-        <div class="login-header">로그인</div>
+        <div class="login-header">
+            <!-- 뒤로가기 아이콘 -->
+            <img src="../assets/chevron-left.svg" alt="뒤로가기" @click="goBack" />
+            <p class="p01">로그인</p>
+        </div>
         
         <!-- 어웨이커스 로고 -->
         <img src="../assets/어웨이커스.svg" alt="AWAKERS LOGO" class="login-image" />
         
-        <!-- 이름 입력란 -->
-        <label for="name">이름</label>
-        <input type="text" id="name" v-model="name" />
 
-        <!-- 전화번호 입력란 -->
-        <label for="phone">전화번호</label>
-        <input type="text" id="phone" v-model="phoneNumber" />
+        <div class="inputData">
 
+            <!-- 이름 입력란 -->
+            <input type="text" id="name" placeholder="이름" class="bottom-border" v-model="name"/>
+
+            <!-- 전화번호 입력란 -->
+            <input type="text" id="phone" placeholder="연락처" class="bottom-border" v-model="phoneNumber" />
+        
+        </div>
+        
+        <!-- 로그인 버튼 -->
         <button @click="login">로그인 하기</button>
     </div>
 </template>
@@ -47,13 +52,22 @@ export default {
 
 .login-header{
     text-align: center;
-    font-size: 24px;
+    font-size: 15px;
     margin-top: 20px;
+    display: flex;
+
+    .p01{
+        margin: 5px 5px 5px -20px;
+        height:100%;
+        width: 100%;
+
+    }
+    
 }
 
 .login-image{
     display: block;
-    margin: 20px auto;
+    margin: 50px auto;
     max-width: 100%;
     height: auto;
 }
@@ -63,19 +77,33 @@ label{
     margin-top: 10px;
 }
 
-input{
+.inputData{
+    line-height: 60px;
+    input{
     width: 100%;
     padding: 5px;
     margin-bottom: 10px;
+    }
+
+    input:focus{
+        outline: none;
+    }
+}
+
+.bottom-border{
+    border-width: 0 0 1px;
 }
 
 button{
-    width: 100%;
+    width: 350px;
     padding: 10px;
-    background-color: #68776a;
+    background-color: #606761;
     color: white;
     border: none;
+    border-radius: 8px;
     cursor: pointer;
+    position: absolute;
+    bottom: 0;
 }
 button:hover{
     background-color: #171a17;
