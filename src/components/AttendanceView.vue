1563307fc6ecf5ad8fbda16f7aa5394c05e582be
james4510 <james451055@gmail.com>
2024-01-29 15:25:17 +0900

    Attendance Page update by Yong Geun [2024.1.29]

--- src/components/AttendanceView.vue
+++ src/components/AttendanceView.vue
@@ -2,16 +2,19 @@
     <body>
         <div class="wrap">
             <div class="header">
+                
                 <!-- 뒤로 가기 버튼 -->
                 <div class="backButton">
                     <a href="javascript:history.back()" class="title"><img src="../assets/chevron-left.svg" alt="뒤로가기"/></a>
                 </div>
+                
                 <!-- 페이지 제목-->
                 <p class="pageTitle">출석관리</p>
             </div>
             <div class="container">
                 <div class="menu">
                     <div class="filterMenu">
+                        
                         <!-- 페이지 소제목-->
                         <div class="title">일정 선택</div>    
 
@@ -26,6 +29,7 @@
 
                 <!-- 필터링 버튼 -->
                 <div class="filters">
+                    
                     <!-- 필터링 버튼 1 (전체, 예배별, 행사별, 봉사별)-->
                     <div class="filterButton">
                         <div class="selectTypeButton">
@@ -37,12 +41,12 @@
                     </div>
 
                     <!-- 필터링 버튼 2 -->
-                    <form action="#">
-                        <select name="languages" id="lang">
-                            <option value="javascript">날짜순</option>
-                            <option value="php">가나다순</option>
+                    <div class="filterButton2">
+                        <select name="orders" id="order">
+                            <option value="date">날짜순</option>
+                            <option value="lexical">가나다순</option>
                         </select>
-                    </form>
+                    </div>
                 </div>
                 
                 <!-- 컨텐츠 -->
@@ -55,9 +59,11 @@
                                 <div class="attendance">{{ event.attendees }}명 출석</div>
                                 <div class="type">{{ event.type }}</div>
                             </div>
-                            <img src="../assets/options-horizontal.svg" alt="Edit Event" class="edditEvents" />
+                            <div class="editEvents">
+                                <img src="../assets/options-horizontal.svg" alt="Edit Event" />
+                            </div>
                         </div>
-
+                        
                         <!-- 스크롤을 감지하기 위한 이벤트 리스너 -->
                         <div ref="scrollContainer" @scroll="handleScroll" class="scrollContainer"></div>
                     </div>
@@ -190,141 +196,158 @@ body{
             }
         }
 
-    .container{
-        height: 800px;
-        width: 330px;
-        //background: #ccc;
-
-        .menu{
-            font-size: 17px;
+        .container{
+            height: 800px;
             width: 330px;
-            height: 50px;
-            //background: #d3d5f3;
-            float: left;
-            display: flex;
+            //background: #ccc;
 
-            .filterMenu{
-                font-size: 18px;
+            .menu{
+                font-size: 17px;
                 width: 330px;
-                height: 24px;
+                height: 50px;
+                //background: #d3d5f3;
+                float: left;
                 display: flex;
-                .title{
-                    color: rgb(118, 114, 114);
-                    margin-top: 3px;
-                    font-weight: 800;
-                    width: 80px;
+
+                .filterMenu{
+                    font-size: 18px;
+                    width: 330px;
                     height: 24px;
+                    display: flex;
+                
+                    .title{
+                        color: rgb(118, 114, 114);
+                        margin-top: 3px;
+                        font-weight: 800;
+                        width: 80px;
+                        height: 24px;
+                    }
+
+                    .toast{
+                        display: flex;
+                        margin-left: auto;
+                    
+                        .toastButton{
+                            border: none;
+                            background-color: transparent;
+                            cursor: pointer;
+
+                            .addButton{
+                                cursor: pointer;
+                            }
+                        }
+                    }
+                }
+            }
+
+            .filters{
+                
+                .filterButton{
+                    width: 330px;
+                    height: 100px;
+                    
+
+                    .selectTypeButton{
+                        display: flex;
+                        gap: 7px;
+                        border-radius: 5px;
+                        width: 200px;
+                        height: 30px;
+                        
+                        .selectType{
+                            width: 50px;
+                            height: 30px;
+                            border : none;
+                            border-radius : 4px;
+                            color: rgb(91, 88, 88);
+                            cursor: pointer;
+                            //background-color: rgb(204, 198, 198);
+                        }
+
+                        .selectType:hover{
+                            color: white;
+                            background-color: rgb(88, 87, 87);
+                        }
+                    }
+
+                    .filterButton2{
+                        float: left;
+                        margin: 10px;
+                    }
                 }
 
-                .toast{
-                    display: flex;
-                    margin-left: auto;
-                    .toastButton{
-                        border: none;
-                        background-color: transparent;
-                        cursor: pointer;
+            }
+        
+            .contentsContainer{
 
-                        .addButton{
+                .contents{
+                    width: 330px;
+                    height: 700px;
+                    //background: #fffa99;
+                    float: left;
+
+                    .contentsBox{
+                        width: 290px;
+                        height: 50px;
+                        background-color: rgb(223, 219, 219);
+                        border-radius: 10px;
+                        padding: 16px;
+
+                        .contentsTitle{
+                            font-size: 17px;
+                            font-weight: bold;
+                            color: rgb(98, 97, 97);
+                        }
+
+                        .contentsDate{
+                            font-size: 10px;
+                            margin-top: 5px;
+                            color: rgb(132, 129, 129);
+                        }
+
+                        .etc{
+                            margin-top: 10px;
+                            display: flex;
+                            gap: 6px;
+
+                            .attendance{
+                                color: rgb(125, 124, 124);
+                                font-weight: bold;
+                                font-size: 13px;
+                            }
+
+                            .type{
+                                color: gray;
+                                background-color: white;
+                                clear: both;
+                                float: left;
+                                width: 25px;
+                                height: 16px;
+                                border-radius: 6px;
+                                font-weight: 650;
+                                font-size: 10.8px;
+                                display: flex;
+                                flex-direction: column;
+                                justify-content: center;
+                                align-items: center;
+                            }
+                        }
+
+                        .editEvents{
+                            margin-top: -40px;
+                            float: right;
                             cursor: pointer;
                         }
-                    }
-                }
-            }
-        }
 
-        .filterButton{
-            width: 330px;
-            height: 100px;
-            .selectTypeButton{
-                display: flex;
-                gap: 7px;
-                border-radius: 5px;
-                width: 200px;
-                height: 30px;
-                .selectType{
-                    width: 50px;
-                    height: 30px;
-                    border : none;
-                    border-radius : 4px;
-                    color: rgb(91, 88, 88);
-                    cursor: pointer;
-                    //background-color: rgb(204, 198, 198);
-                }
-                .selectType:hover{
-                    color: white;
-                    background-color: rgb(88, 87, 87);
-                }
-            }
-        }
-
-
-        
-        .contentsContainer{
-            .contents{
-                width: 330px;
-                height: 700px;
-                //background: #fffa99;
-                float: left;
-
-                .contentsBox{
-                    width: 290px;
-                    height: 50px;
-                    background-color: rgb(223, 219, 219);
-                    border-radius: 10px;
-                    padding: 16px;
-
-                    .contentsTitle{
-                        font-size: 17px;
-                        font-weight: bold;
-                        color: rgb(98, 97, 97);
-                    }
-
-                    .contentsDate{
-                        font-size: 10px;
-                        margin-top: 5px;
-                        color: rgb(132, 129, 129);
-                    }
-
-                    .etc{
-                        margin-top: 10px;
-                        display: flex;
-                        gap: 6px;
-                        .attendance{
-                            color: rgb(125, 124, 124);
-                            font-weight: bold;
-                            font-size: 13px;
-                        }
-
-                        .type{
-                            color: gray;
-                            background-color: white;
-                            clear: both;
-                            float: left;
-                            width: 25px;
-                            height: 16px;
-                            border-radius: 6px;
-                            font-weight: 650;
-                            font-size: 10.8px;
-                            display: flex;
-                            flex-direction: column;
-                            justify-content: center;
-                            align-items: center;
+                        .scrollContainer{
+                            height: 300px;
+                            overflow-y: scroll;
                         }
                     }
-
-                    .edditEvents{
-                        display: flex;
-                    }
-
-                    .scrollContainer{
-                        height: 300px;
-                        overflow-y: scroll;
-                    }
                 }
-            }
-        }  
+            }  
+        }
     }
-}
 }
 
 
